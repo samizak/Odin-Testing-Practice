264dc8ad89fb68ffe221a4db5e2d1f2887162e86
Sami Zakir Ahmed <54m1z4k@gmail.com>
2023-07-01 17:34:41 +0100

    Added more Tests for capitalise function

--- test/capitalize.test.js
+++ test/capitalize.test.js
@@ -4,4 +4,12 @@ describe("Capitalize first character of the string.", () => {
   test("Name", () => {
     expect(capitalizeFirstLetter("sami")).toBe("Sami");
   });
+
+  test("Is Undefined", () => {
+    expect(capitalizeFirstLetter()).toBe("");
+  });
+
+  test("Is Null", () => {
+    expect(capitalizeFirstLetter(null)).toBe("");
+  });
 });
